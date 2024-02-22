7acce2fcfb96fbdd0b4a14a2232f9a8633a82af8
Tim <t27.jones@connect.qut.edu.au>
2024-02-22 23:16:01 +1000

    Update placeholder image

--- components/Pages/Home/Header/index.tsx
+++ components/Pages/Home/Header/index.tsx
@@ -11,12 +11,14 @@ interface HeaderProps {
 const Header: FC<HeaderProps> = ({ onClick }) => {
   return (
     <section className="relative overflow-hidden">
-      <OptimisedImage
-        src="/img/home/background.jpg"
-        alt=""
-        layout="cover"
-        className="inset-0 xxs:absolute"
-      />
+      <div className="absolute inset-0 flex items-center justify-end pr-40">
+        <OptimisedImage
+          src="/img/home/background-placeholder.jpg"
+          alt=""
+          layout="cover"
+          className="w-[600px] overflow-hidden rounded-md"
+        />
+      </div>
       <Container className="flex h-svh min-h-[600px] flex-col items-start justify-center">
         <header className="flex flex-col items-start gap-y-6">
           <h1 className="text-display-m">{siteName} Claim Portal</h1>
