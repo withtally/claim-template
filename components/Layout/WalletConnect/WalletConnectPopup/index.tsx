53debfe0fabe43cd2d096bd5ed5ca19f18e643e5
Nazar-Brunarskyi <3nnazar836@gmail.com>
2024-03-08 10:56:48 +0200

    move disconnect popup

--- components/Layout/WalletConnect/WalletConnectPopup/index.tsx
+++ components/Layout/WalletConnect/WalletConnectPopup/index.tsx
@@ -1,13 +1,32 @@
 import { useWalletConnectContext } from '../../../../contexts/WalletConnectContext'
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 import Cross from '../../../../public/img/icons/cross.svg'
 import Back from '../../../../public/img/icons/back.svg'
 import WalletsList from './WalletsList'
 import ChainsList from '~/components/Layout/WalletConnect/WalletConnectPopup/ChainsList'
+import { WalletDisconnectPopup } from '../WalletDisconnectPopup'
+import { useAccount, useDisconnect } from 'wagmi'
 
 export const WalletConnectPopup: FC = () => {
   const [isChainsShowed, setIsChainsShowed] = useState<boolean>(false)
   const { setConnectConnectPopupVisibility } = useWalletConnectContext()
+  const { address, connector, isConnected } = useAccount()
+  const { disconnect } = useDisconnect()
+
+  const doDisconnect = useCallback(() => {
+    disconnect(
+      {},
+      {
+        onSuccess: () => {
+          setConnectConnectPopupVisibility(false);
+        },
+      }
+    );
+  }, []);
+
+  const onClose = useCallback(() => {
+    setConnectConnectPopupVisibility(false);
+  }, []);
 
   return (
     <div
@@ -25,41 +44,41 @@ export const WalletConnectPopup: FC = () => {
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="relative my-8 w-full max-w-lg transform overflow-hidden rounded-2xl bg-blue-grey text-left shadow-xl transition-all p-[16px]"
+            className="relative my-8 w-full max-w-md transform overflow-hidden rounded-2xl bg-blue-grey p-[16px] text-left shadow-xl transition-all"
           >
-            <div className="flex items-center justify-between mb-[32px]">
-              <h1 className="text-xl font-extrabold font-semibold">Connect Wallet</h1>
+            {isConnected ? (
+              <WalletDisconnectPopup
+                address={address}
+                connector={connector}
+                onDisconnect={doDisconnect}
+                onClose={onClose}
+              />
+            ) : (
+              <>
+                <div className="mb-[32px] flex items-center justify-between">
+                  <h1 className="text-xl font-extrabold font-semibold">Connect Wallet</h1>
 
-              {
-                isChainsShowed ? (
-                  <Back
-                    className="size-3.5 cursor-pointer"
-                    onClick={() => setIsChainsShowed(false)}
-                  />
-                ) : (
-                  <Cross
-                    className="size-3.5 cursor-pointer"
-                    onClick={() => setConnectConnectPopupVisibility(false)}
-                  />
-                )
-              }
-
-            </div>
-            <div className="mb-[32px]">
-              {
-                isChainsShowed ? (
-                  <ChainsList />
-                ) : (
-                  <WalletsList
-                    setIsChainsShowed={setIsChainsShowed}
-                  />
-                )
-              }
-            </div>
-            <div className="flex">
-              <p className="mr-[8px]">Don't have a wallet?</p>
-              <a href="https://ethereum.org/en/wallets/">Learn more</a>
-            </div>
+                  {isChainsShowed ? (
+                    <Back
+                      className="size-3.5 cursor-pointer"
+                      onClick={() => setIsChainsShowed(false)}
+                    />
+                  ) : (
+                    <Cross
+                      className="size-3.5 cursor-pointer"
+                      onClick={() => setConnectConnectPopupVisibility(false)}
+                    />
+                  )}
+                </div>
+                <div className="mb-[32px]">
+                  {isChainsShowed ? <ChainsList /> : <WalletsList setIsChainsShowed={setIsChainsShowed} />}
+                </div>
+                <div className="flex">
+                  <p className="mr-[8px]">Don't have a wallet?</p>
+                  <a href="https://ethereum.org/en/wallets/">Learn more</a>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
--- components/Layout/WalletConnect/WalletDisconnectPopup/index.tsx
+++ components/Layout/WalletConnect/WalletDisconnectPopup/index.tsx
@@ -1,6 +1,6 @@
 import { useWalletConnectContext } from '../../../../contexts/WalletConnectContext'
 import { FC } from 'react'
-import { useAccount, useDisconnect } from 'wagmi'
+import { Connector, useAccount, useDisconnect } from 'wagmi'
 import Cross from '../../../../public/img/icons/cross.svg'
 import Copy from '../../../../public/img/icons/copy.svg'
 import { WalletIcon } from '../WalletIcon'
@@ -8,71 +8,100 @@ import { WalletConnectors } from '~/types/wallet-connectors'
 import { shortenAddress } from '~/utils/common'
 import Button from '../../Button'
 
-export const WalletDisconnectPopup: FC = () => {
-  const { setConnectConnectPopupVisibility } = useWalletConnectContext()
-  const { address, connector } = useAccount()
-  const connectorName = connector?.name
-  const { disconnect } = useDisconnect()
+interface Props {
+  onDisconnect: () => void
+  address: `0x${string}`
+  connector: Connector
+  onClose: () => void
+}
 
-  const doDisconnect = () => {
-    disconnect({}, {
-      onSuccess: () => {
-        console.log('dis');
-        setConnectConnectPopupVisibility(false)
-      }
-    })
-  }
+export const WalletDisconnectPopup: FC<Props> = ({ onDisconnect, address, connector, onClose }) => {
+  const connectorName = connector?.name
 
   return (
-    <div
-      className="relative z-50"
-      aria-labelledby="modal-title"
-      role="dialog"
-      aria-modal="true"
-    >
-      <div className="fixed inset-0 bg-blue-grey/70 backdrop-blur-lg"></div>
+    <>
+      <div className="flex items-center justify-between border-b border-gray-500 p-[16px]">
+        <h1 className="text-xl font-extrabold font-semibold">Connected</h1>
 
-      <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div
-          onClick={() => setConnectConnectPopupVisibility(false)}
-          className="flex min-h-full items-end items-center justify-center p-0 p-4 text-center"
-        >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="relative my-8 w-full max-w-sm transform overflow-hidden rounded-2xl bg-blue-grey text-left shadow-xl transition-all"
-          >
-            <div className="flex items-center justify-between border-b border-gray-500 p-[16px]">
-              <h1 className="text-xl font-extrabold font-semibold">Connect Wallet</h1>
-
-              <Cross
-                className="size-3.5 cursor-pointer"
-                onClick={() => setConnectConnectPopupVisibility(false)}
-              />
-            </div>
-
-            <div className="flex flex-col items-center justify-between gap-6 p-[16px]">
-              <WalletIcon
-                className="size-14"
-                walletName={connectorName as WalletConnectors}
-              />
-
-              <div className='flex gap-[10px] items-center justify-between'>
-                <span>{address && shortenAddress(address)}</span>
-                <button
-                  className=' hover:bg-black hover:bg-opacity-20 p-2 rounded'
-                  onClick={() => {
-                    navigator.clipboard.writeText(address)
-                  }}
-                >
-                  <Copy className='size-4' />
-                </button>
-              </div>
-
-              <Button onClick={doDisconnect}>Disconect</Button>
-            </div>
-          </div>
-        </div>
+        <Cross
+          className="size-3.5 cursor-pointer"
+          onClick={onClose}
+        />
       </div>
-    </div>
+
+      <div className="flex flex-col items-center justify-between gap-6 p-[16px]">
+        <WalletIcon
+          className="size-14"
+          walletName={connectorName as WalletConnectors}
+        />
+
+        <div className="flex items-center justify-between gap-[10px]">
+          <span>{address && shortenAddress(address)}</span>
+          <button
+            className=" rounded p-2 hover:bg-black hover:bg-opacity-20"
+            onClick={() => {
+              navigator.clipboard.writeText(address)
+            }}
+          >
+            <Copy className="size-4" />
+          </button>
+        </div>
+
+        <Button onClick={onDisconnect}>Disconect</Button>
+      </div>
+    </>
   )
+
+  // return (
+  //   <div
+  //     className="relative z-50"
+  //     aria-labelledby="modal-title"
+  //     role="dialog"
+  //     aria-modal="true"
+  //   >
+  //     <div className="fixed inset-0 bg-blue-grey/70 backdrop-blur-lg"></div>
+
+  //     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+  //       <div
+  //         onClick={() => setConnectConnectPopupVisibility(false)}
+  //         className="flex min-h-full items-end items-center justify-center p-0 p-4 text-center"
+  //       >
+  //         <div
+  //           onClick={(e) => e.stopPropagation()}
+  //           className="relative my-8 w-full max-w-sm transform overflow-hidden rounded-2xl bg-blue-grey text-left shadow-xl transition-all"
+  //         >
+  //           <div className="flex items-center justify-between border-b border-gray-500 p-[16px]">
+  //             <h1 className="text-xl font-extrabold font-semibold">Connect Wallet</h1>
+
+  //             <Cross
+  //               className="size-3.5 cursor-pointer"
+  //               onClick={() => setConnectConnectPopupVisibility(false)}
+  //             />
+  //           </div>
+
+  //           <div className="flex flex-col items-center justify-between gap-6 p-[16px]">
+  //             <WalletIcon
+  //               className="size-14"
+  //               walletName={connectorName as WalletConnectors}
+  //             />
+
+  //             <div className="flex items-center justify-between gap-[10px]">
+  //               <span>{address && shortenAddress(address)}</span>
+  //               <button
+  //                 className=" rounded p-2 hover:bg-black hover:bg-opacity-20"
+  //                 onClick={() => {
+  //                   navigator.clipboard.writeText(address)
+  //                 }}
+  //               >
+  //                 <Copy className="size-4" />
+  //               </button>
+  //             </div>
+
+  //             <Button onClick={doDisconnect}>Disconect</Button>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 }
