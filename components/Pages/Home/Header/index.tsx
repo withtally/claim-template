6fdb0bf7220efd665d85690edd3135094e3530fb
Nazar-Brunarskyi <3nnazar836@gmail.com>
2024-03-18 11:59:39 +0200

    deployment test

--- components/Pages/Home/Header/index.tsx
+++ components/Pages/Home/Header/index.tsx
@@ -21,7 +21,7 @@ const Header: FC<HeaderProps> = ({ onClick }) => {
       </div>
       <Container className="flex h-svh min-h-[600px] flex-col items-start justify-center pt-16">
         <header className="flex flex-col items-start gap-y-6">
-          <h1 className="text-display-m">{siteName} Claim Portal</h1>
+          <h1 className="text-display-m">{siteName} Claim Portal deployment test</h1>
           <p className="text-title text-gray-400">Check your eligibility to claim tokens</p>
           <Button onClick={onClick}>Check eligibility</Button>
         </header>
