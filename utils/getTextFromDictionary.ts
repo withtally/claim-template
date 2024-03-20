5d35a00e4697ea8d0bdbd1ea3dac866d766a9df0
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-03-20 23:51:59 +0200

    Basic setup for dictionary config and helper function

---
+++ utils/getTextFromDictionary.ts
@@ -0,0 +1,16 @@
+import dictionary from '~/config/dictionaryConfig.json'
+
+export const getTextFromDictionary = (path: string)=> {
+  const arrOfKeys = path.split('_')
+  console.log(dictionary)
+  let item = dictionary;
+  for(const key of arrOfKeys){
+    item = item[key];
+  }
+  if(!item || typeof item !== 'string'){
+    console.error('This key do not exist!')
+    return;
+  }
+  return item;
+
+}
