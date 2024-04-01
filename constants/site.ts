255cc8f6165406cd5fa7b0122e1f758132fd4238
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-04-01 12:43:41 +0300

    Set sepolia as default chain

--- constants/site.ts
+++ constants/site.ts
@@ -1,3 +1,3 @@
-export const siteName = process.env.NEXT_PUBLIC_PROJECT_NAME || 'Hedgey'
-export const WALLLET_CONNECT_ID = 'walletConnect'
-export const chainToUse = 'avalanche'
+export const siteName = process.env.NEXT_PUBLIC_PROJECT_NAME || "Hedgey";
+export const WALLLET_CONNECT_ID = "walletConnect";
+export const chainToUse = "sepolia";
