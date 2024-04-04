b98c295b5caaa2019db4d897085dce8d1de7b07d
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-04-04 15:46:39 +0300

    Revert "Debugging connection error in metamask browser. Delete this commit after debug"
    This reverts commit 67c32d1f6e6d0e3e418de6b5a6d243ba858f3def.

--- components/Layout/WalletConnect/WalletConnectPopup/index.tsx
+++ components/Layout/WalletConnect/WalletConnectPopup/index.tsx
@@ -5,40 +5,26 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay,
+  ModalOverlay
 } from "@chakra-ui/modal";
 import { Button } from "@chakra-ui/react";
-import { FC, MouseEvent, useCallback } from "react";
-import {
-  Connector,
-  useAccount,
-  useConnect,
-  useDisconnect,
-  useSwitchChain,
-} from "wagmi";
-import { getChain } from "~/config/wagmi/getChain";
-import { chainToUse } from "~/constants/site";
+import { FC, MouseEvent, useCallback, useEffect } from "react";
+import { Connector, useAccount, useConnect, useDisconnect, useSwitchChain } from "wagmi";
+import { chainToUse, WALLLET_CONNECT_ID } from "~/constants/site";
 import useCustomToasters from "~/hooks/useToasters";
 import { showErrorMessage } from "~/utils/getErrorMessage";
 import Cross from "../../../../public/img/icons/cross.svg";
 import { DisconnectContent } from "./components/DisconnectContent";
 import WalletsList from "./components/WalletsList";
+import { getChain } from "~/config/wagmi/getChain";
 
 interface Props {
   isOpen: boolean;
   onCloseConnectPopup: () => void;
 }
 
-export const WalletConnectPopup: FC<Props> = ({
-  isOpen,
-  onCloseConnectPopup,
-}) => {
-  const {
-    address,
-    connector,
-    isConnected,
-    chainId: currentChain,
-  } = useAccount();
+export const WalletConnectPopup: FC<Props> = ({ isOpen, onCloseConnectPopup }) => {
+  const { address, connector, isConnected, chainId: currentChain } = useAccount();
   const { connectors, connect } = useConnect();
   const { disconnect } = useDisconnect();
   const { errorToast } = useCustomToasters();
@@ -55,19 +41,20 @@ export const WalletConnectPopup: FC<Props> = ({
         {
           onSuccess: async (data) => {
             if (connector.id === "walletConnect" && data.chainId !== chain.id) {
-              switchChain({ chainId: chain.id });
+              switchChain(
+                { chainId: chain.id }
+              );
             }
             onCloseConnectPopup();
           },
           onError: (error: any) => {
-            alert(JSON.stringify(error));
             showErrorMessage({
               errorCode: error?.cause?.code,
               message: error.message,
-              toast: errorToast,
+              toast: errorToast
             });
-          },
-        },
+          }
+        }
       );
     };
   }, []);
@@ -78,19 +65,20 @@ export const WalletConnectPopup: FC<Props> = ({
       {
         onSuccess: () => {
           onCloseConnectPopup();
-        },
-      },
+        }
+      }
     );
   }, []);
 
   return (
-    <Modal isOpen={isOpen} onClose={onCloseConnectPopup}>
+    <Modal
+      isOpen={isOpen}
+      onClose={onCloseConnectPopup}
+    >
       <ModalOverlay />
       <ModalContent className="max-md:mx-[20px]">
         <ModalHeader>
-          <h1 className="text-xl font-semibold">
-            {isConnected ? "Connected" : "Connect Wallet"}
-          </h1>
+          <h1 className="text-xl font-semibold">{isConnected ? "Connected" : "Connect Wallet"}</h1>
         </ModalHeader>
         <ModalCloseButton>
           <Cross className="size-3.5" />
