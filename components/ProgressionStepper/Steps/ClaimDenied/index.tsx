49950d8957ed76e371e422b81dd24356f3eaf4f3
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-04-23 16:31:23 +0300

    Finished UI and logic for claim denied step

--- components/ProgressionStepper/Steps/ClaimDenied/index.tsx
+++ components/ProgressionStepper/Steps/ClaimDenied/index.tsx
@@ -1,18 +1,38 @@
 import { InfoOutlineIcon } from "@chakra-ui/icons";
-import { Button, Input } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 import cx from "classnames";
-import { FC } from "react";
+import { FC, useState } from "react";
 import Container from "~/components/Layout/Container";
+import { Input } from "~/components/Layout/Input";
 import CustomTooltip from "~/components/Layout/Tooltip";
 import { UIconfig } from "~/config/UIconfig";
-import { ClaimStatusEnum } from '~/types/common'
+import useCustomToasters from "~/hooks/useToasters";
+import { Address, ClaimStatusEnum } from "~/types/common";
+import { useClaimContext } from "../../../../contexts/ClaimContext";
 
 interface InitialScreenProps {}
 
-// const mockedValue = ClaimStatusEnum.NOT_ELIGIBLE;
-const mockedValue = ClaimStatusEnum.ALREADY_CLAIMED;
-
 const ClaimDeniedScreen: FC<InitialScreenProps> = () => {
+  const [address, setAddress] = useState("");
+  const [infoBlock, setInfoBlock] = useState<ClaimStatusEnum>(
+    ClaimStatusEnum.UNKNOWN,
+  );
+
+  const { infoToast } = useCustomToasters();
+
+  const handleCheckButtonClick = async () => {
+    setInfoBlock(ClaimStatusEnum.UNKNOWN);
+    if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
+      infoToast({ title: "Please enter a valid ETH address" });
+      return;
+    }
+    const isAbleToClaim = await checkEligibilityOfAnotherWallet(
+      address as Address,
+    );
+    setInfoBlock(isAbleToClaim);
+  };
+  const { claimStatus, checkEligibilityOfAnotherWallet, isCheckingEligibility } = useClaimContext();
+
   return (
     <div className="inline snap-start transition-opacity ">
       <section className="mt-[64px]  h-[calc(100svh-64px)] w-[100svw] overflow-auto flex flex-col">
@@ -28,20 +48,16 @@ const ClaimDeniedScreen: FC<InitialScreenProps> = () => {
               )}
             >
               <h3 className="flex-1 w-full text-subheading mb-1 text-center">
-                {mockedValue === ClaimStatusEnum.NOT_ELIGIBLE
+                {claimStatus === ClaimStatusEnum.NOT_ELIGIBLE
                   ? "Sorry you aren’t eligible"
                   : "Sorry you already claimed tokens"}
               </h3>
               <hr className="border-1 my-4 w-full" />
-              <div
-                className={cx(
-                  "mb-4 flex w-full flex-grow flex-col items-start",
-                )}
-              >
-                {mockedValue === ClaimStatusEnum.NOT_ELIGIBLE && (
+              <div className={cx("flex w-full flex-grow flex-col items-start")}>
+                {claimStatus === ClaimStatusEnum.NOT_ELIGIBLE && (
                   <h3 className="text-caption">Eligibility Criteria:</h3>
                 )}
-                {mockedValue === ClaimStatusEnum.NOT_ELIGIBLE && (
+                {claimStatus === ClaimStatusEnum.NOT_ELIGIBLE && (
                   <ul className="mb-6 text-caption inline-flex flex-col items-center uppercase">
                     {UIconfig.eligibilityCriterias.map((criteria, i) => (
                       <li
@@ -57,7 +73,7 @@ const ClaimDeniedScreen: FC<InitialScreenProps> = () => {
                     ))}
                   </ul>
                 )}
-                {mockedValue === ClaimStatusEnum.ALREADY_CLAIMED && (
+                {claimStatus === ClaimStatusEnum.ALREADY_CLAIMED && (
                   <>
                     <p className="mb-4">
                       You may try another wallet to claim and delegate tokens.
@@ -69,10 +85,35 @@ const ClaimDeniedScreen: FC<InitialScreenProps> = () => {
                   Check another wallet for eligibility:
                 </h2>
                 <div className="flex flex-col sm:flex-row gap-y-3">
-                  <Input placeholder="Enter wallet address" mr={2} />
-                  <Button size="md" className="px-2" px={8}>
+                  <Input
+                    placeholder="Enter wallet address"
+                    onChange={setAddress}
+                    isInvalid={infoBlock === ClaimStatusEnum.NOT_ELIGIBLE || infoBlock === ClaimStatusEnum.ALREADY_CLAIMED}
+                    isSuccess={infoBlock === ClaimStatusEnum.ELIGIBLE}
+                    className="mr-2"
+                  />
+                  <Button
+                    size="md"
+                    className="px-2"
+                    px={8}
+                    isLoading={isCheckingEligibility}
+                    onClick={handleCheckButtonClick}
+                  >
                     Check eligibility
                   </Button>
+                </div>
+                <div
+                  className={cx(
+                    "w-full overflow-hidden transition-[max-height,opacity,margin] duration-300",
+                    {
+                      "max-h-0 opacity-0":
+                        infoBlock === ClaimStatusEnum.UNKNOWN,
+                      "mt-4 max-h-16 overflow-visible":
+                        infoBlock !== ClaimStatusEnum.UNKNOWN,
+                    },
+                  )}
+                >
+                  <InfoBlockBody eligibleStatus={infoBlock} />
                 </div>
               </div>
             </div>
@@ -81,6 +122,19 @@ const ClaimDeniedScreen: FC<InitialScreenProps> = () => {
       </section>
     </div>
   );
+};
+
+const InfoBlockBody: FC<{ eligibleStatus: ClaimStatusEnum }> = ({
+  eligibleStatus,
+}) => {
+  const info = {
+    [ClaimStatusEnum.ALREADY_CLAIMED]:
+      <p className="text-errorColor">Sorry, you already claimed tokens via this wallet</p>,
+    [ClaimStatusEnum.NOT_ELIGIBLE]: <p className="text-errorColor">Sorry this wallet is not eligible</p>,
+    [ClaimStatusEnum.ELIGIBLE]: <p className="text-green">This wallet is eligible. </p>,
+  };
+
+  return info[eligibleStatus];
 };
 
 export default ClaimDeniedScreen;
