508c83a4837fd170c33d4a6770eb83785062d363
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-04-23 16:32:41 +0300

    Made different steps for different claimStatus

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useClaimContext } from "contexts/ClaimContext";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import AnimateOnUpdate from "~/components/Layout/AnimateOnUpdate";
 import Header from "~/components/Pages/Home/Header";
 import ProgessionStepper from "~/components/ProgressionStepper";
@@ -7,6 +7,8 @@ import DelegateStep from "~/components/ProgressionStepper/Steps/Delegate";
 import InitialScreen from "~/components/ProgressionStepper/Steps/Initial";
 import { SEO } from "~/components/SEO";
 import ClaimStep from "~/components/ProgressionStepper/Steps/Claim";
+import { ClaimStatusEnum } from "~/types/common"
+import ClaimDenied from "~/components/ProgressionStepper/Steps/ClaimDenied"
 
 const HireReactDeveloperPage: FC = () => {
 
@@ -15,9 +17,16 @@ const HireReactDeveloperPage: FC = () => {
     isClaimStepperVisible,
     isCheckingEligibility,
     handleCheckEligibility,
+    claimStatus
   } = useClaimContext();
 
-  const components = [InitialScreen, DelegateStep, ClaimStep];
+  const components = useMemo(() => {
+    if(claimStatus === ClaimStatusEnum.ELIGIBLE){
+      return [InitialScreen, DelegateStep, ClaimStep]
+    }else{
+      return [ClaimDenied]
+    }
+  },[claimStatus]);
   return (
     <>
       <SEO title="Home" />
