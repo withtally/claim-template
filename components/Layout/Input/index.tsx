a198c960f0e0dfaee5d3572395caab565bd2c861
Maksym Paprotskyi <mp@codegeeks.solutions>
2024-04-23 16:30:50 +0300

    Input changes

--- components/Layout/Input/index.tsx
+++ components/Layout/Input/index.tsx
@@ -1,13 +1,21 @@
-import { Input as ChakraInput, InputGroup, InputLeftElement } from '@chakra-ui/react'
-import { FC } from 'react'
+import {
+  Input as ChakraInput,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { UIconfig } from "~/config/UIconfig";
 
 interface Props {
-  inputLeftElement?: any
-  placeholder: string
-  value?: string
-  onChange?: (newValue: string) => void
-  className?: string
-  inputGroupClassName?: string
+  inputLeftElement?: any;
+  placeholder: string;
+  value?: string;
+  onChange?: (newValue: string) => void;
+  className?: string;
+  inputGroupClassName?: string;
+  isInvalid?: boolean;
+  isSuccess?: boolean;
+
 }
 
 export const Input: FC<Props> = ({
@@ -17,20 +25,35 @@ export const Input: FC<Props> = ({
   onChange,
   className,
   inputGroupClassName,
+  isInvalid,
+  isSuccess
 }) => {
+
+  const successStyles = {
+    borderColor: `${UIconfig.colors.selected} !important`,
+    boxShadow: `0 0 0 1px ${UIconfig.colors.selected} !important`,
+    _focus: {
+      borderColor: `${UIconfig.colors.selected} !important`,
+      boxShadow: `0 0 0 1px ${UIconfig.colors.selected} !important`,
+    },
+  }
   return (
     <InputGroup className={inputGroupClassName}>
-      {inputLeftElement && <InputLeftElement>{inputLeftElement}</InputLeftElement>}
+      {inputLeftElement && (
+        <InputLeftElement>{inputLeftElement}</InputLeftElement>
+      )}
       <ChakraInput
         value={value}
         className={className}
         placeholder={placeholder}
+        isInvalid={isInvalid}
+        sx={isSuccess && successStyles}
         onChange={(e) => {
-          onChange && onChange(e.target.value)
+          onChange && onChange(e.target.value);
         }}
         // TODO: change to error color
         errorBorderColor="crimson"
       />
     </InputGroup>
-  )
-}
+  );
+};
