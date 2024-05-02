2ec60e16b4ab55a725a287407ea27cab75ade2a8
Nazar-Brunarskyi <3nnazar836@gmail.com>
2024-05-02 15:43:42 +0300

    remove Hedgey's name from everywhere

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,24 @@
-import { useClaimContext } from "contexts/ClaimContext";
-import React, { FC, useMemo } from 'react'
+import { FC } from "react";
 import AnimateOnUpdate from "~/components/Layout/AnimateOnUpdate";
 import Header from "~/components/Pages/Home/Header";
 import ProgessionStepper from "~/components/ProgressionStepper";
-import ClaimStep from "~/components/ProgressionStepper/Steps/Claim";
-import ClaimDenied from "~/components/ProgressionStepper/Steps/ClaimDenied";
-import DelegateStep from "~/components/ProgressionStepper/Steps/Delegate";
-import InitialScreen from "~/components/ProgressionStepper/Steps/Initial";
 import { SEO } from "~/components/SEO";
-import { ClaimStatusEnum } from "~/types/common";
-import { useWalletConnectContext } from '../contexts/WalletConnectContext'
-import ClaimSuccess from '~/components/ProgressionStepper/Steps/ClaimSuccess'
-import { useAccount } from 'wagmi'
-import { useHomePageLogic } from '~/hooks/useHomePageLogic'
+import { useHomePageLogic } from "~/hooks/useHomePageLogic";
 
-// TODO: Rename component
+export const metadata = {
+  icons: {
+    icon: "../punlic/favicon.png", // /public path
+  },
+};
+
 const HomePage: FC = () => {
-
   const {
     areDataFetched,
     isClaimStepperVisible,
     isCheckingEligibility,
     proofs,
     components,
-    checkEligibility
+    checkEligibility,
   } = useHomePageLogic();
 
   return (
